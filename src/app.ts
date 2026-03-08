495dc36a7983d740180826d419b6bccfe700e3bd
said <saidghoul2000@gmail.com>
2026-03-08 17:35:43 +0100

    Allow  Vite dev server

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,16 @@
+import cors from "cors";
 import express from "express";
 import authRoutes from "./routes/auth.routes.js";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Allow  Vite dev server
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 
 // Middleware to parse JSON
 app.use(express.json());
